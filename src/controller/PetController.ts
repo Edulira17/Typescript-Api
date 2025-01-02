5d3bbc8d9bb8b200a129ce14920adf6c5cba32f2
Eduardo Lira <eduardoliracunha17@gmail.com>
2025-01-02 14:49:24 -0300

    instalando ORM para fazer a conexão com o banco de dados.

--- src/controller/PetController.ts
+++ src/controller/PetController.ts
@@ -5,21 +5,29 @@ import EnumEspecie from "../enum/EnumEspecie";
 
 let listaDePets: TipoPet[] = [];
 
+let id = 0;
+function geraId() {
+  id = id + 1;
+  return id;
+}
+
 export default class PetController {
   // creates new pet
   criaPet(req: Request, res: Response) {
-    const { id, especie, nome, idade, adotado } = <TipoPet>req.body;
+    const { especie, nome, dataDeNascimento, adotado } = <TipoPet>req.body;
+
+    
 
     if (!Object.values(EnumEspecie).includes(especie as EnumEspecie)) {
       return res.status(400).json({ erro: "Especie invalida" });
     }
 
-    const novoPet: TipoPet = { 
-      id,
-      especie, 
-      nome, 
-      idade, 
-      adotado 
+    const novoPet: TipoPet = {
+      id: geraId(),
+      especie,
+      nome,
+      dataDeNascimento,
+      adotado,
     };
 
     listaDePets.push(novoPet);
@@ -35,7 +43,7 @@ export default class PetController {
   // updates pet
   atualizaPet(req: Request, res: Response) {
     const { id } = req.params;
-    const { adotado, especie, idade, nome } = req.body as TipoPet;
+    const { adotado, especie, dataDeNascimento, nome } = req.body as TipoPet;
 
     const pet = listaDePets.find((pet) => pet.id === Number(id));
 
@@ -44,7 +52,7 @@ export default class PetController {
     }
 
     pet.nome = nome;
-    pet.idade = idade;
+    pet.dataDeNascimento = dataDeNascimento;
     pet.especie = especie;
     pet.adotado = adotado;
     return res.status(200).json(pet);
