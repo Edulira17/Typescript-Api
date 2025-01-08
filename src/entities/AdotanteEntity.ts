6f5a0864a8908ef4f108a4078c49c49c16fb9881
Eduardo Lira <eduardoliracunha17@gmail.com>
2025-01-08 10:28:56 -0300

    Relacionamento do adotante com o endereço concluido

--- src/entities/AdotanteEntity.ts
+++ src/entities/AdotanteEntity.ts
@@ -1,4 +1,10 @@
-import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 import EnderecoEntity from "./EnderecoEntity";
 
 @Entity()
@@ -11,19 +17,23 @@ export default class AdotanteEntity {
   senha: string;
   @Column()
   celular: string;
-  @Column({nullable: true})
+  @Column({ nullable: true })
   foto?: string;
 
-  @OneToOne(() => EnderecoEntity, {nullable: true, cascade: true, eager: true})
-  @Column({nullable: true})
-  endereco?: string;
+  @OneToOne(() => EnderecoEntity, {
+    nullable: true,
+    cascade: true,
+    eager: true,
+  })
+  @JoinColumn()
+  endereco?: EnderecoEntity;
 
   constructor(
     nome: string,
     senha: string,
     celular: string,
     foto?: string,
-    endereco?: string
+    endereco?: EnderecoEntity
   ) {
     this.nome = nome;
     this.senha = senha;
