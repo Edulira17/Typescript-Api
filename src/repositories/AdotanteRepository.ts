3993b923ceaec66565b4d70d026ec49829550bc2
Eduardo Lira <eduardoliracunha17@gmail.com>
2025-01-08 10:55:43 -0300

    Atualizando o endereço de um adotante

--- src/repositories/AdotanteRepository.ts
+++ src/repositories/AdotanteRepository.ts
@@ -1,6 +1,7 @@
 import { Repository } from "typeorm";
 import AdotanteEntity from "../entities/AdotanteEntity";
 import InterfaceAdotanteRepository from "./interfaces/InterfaceAdotanteRepository";
+import EnderecoEntity from "../entities/EnderecoEntity";
 
 export default class AdotanteRepository implements InterfaceAdotanteRepository {
   constructor(private repository: Repository<AdotanteEntity>) {}
@@ -45,21 +46,39 @@ export default class AdotanteRepository implements InterfaceAdotanteRepository {
     try {
       const adotanteToRemove = await this.repository.findOne({ where: { id } });
 
-      if(!adotanteToRemove){
-        return {success: false, message: "Adotante não encontrado"}
+      if (!adotanteToRemove) {
+        return { success: false, message: "Adotante não encontrado" };
       }
 
       await this.repository.remove(adotanteToRemove);
 
-      return {success: true}
+      return { success: true };
     } catch (error) {
       console.log(error);
 
       return {
         success: false,
-        message: "Ocorreu um erro ao tentar excluir o adotante"
-      }
-
+        message: "Ocorreu um erro ao tentar excluir o adotante",
+      };
     }
+  }
+
+  async atualizaEnderecoAdotante(
+    idAdotante: number,
+    endereco: EnderecoEntity
+  ): Promise<{ success: boolean; message?: string }> {
+    const adotante = await this.repository.findOne({
+      where: { id: idAdotante },
+    });
+
+    if (!adotante) {
+      return { success: false, message: "Adotante nao encontrado" };
+    }
+
+    const novoEndereco = new EnderecoEntity(endereco.cidade, endereco.estado);
+    adotante.endereco = novoEndereco;
+    await this.repository.save(adotante);
+
+    return { success: true };
   }
 }
